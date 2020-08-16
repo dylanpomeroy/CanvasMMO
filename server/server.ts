f1509dfc783fc51a121b56e6ec3509a30ef3a918
Dylan Pomeroy <dylan@dylanpomeroy.com>
2020-08-16 17:26:59 -0400

    enhancements to console logs and bug fixes for position updates/sync with server to other clients

--- server/server.ts
+++ server/server.ts
@@ -8,38 +8,53 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-const userPositions = {};
+interface Position {
+  horizontal: number;
+  vertical: number;
+}
+
+interface Player {
+  id: string;
+  position: Position;
+}
+
+const users: { [playerId: string]: Player } = {};
 
 io.on("connection", (socket) => {
-  console.log("a user connected!");
-  socket.broadcast.emit("playerJoined", socket.id);
-  socket.emit("playerPositions", userPositions);
+  console.log(`user ${socket.id} connected!`);
 
   console.log(`user id is ${socket.id}`);
-  userPositions[socket.id] = [0, 0];
+  users[socket.id] = {
+    id: socket.id,
+    position: { horizontal: 0, vertical: 0 },
+  };
+
+  socket.broadcast.emit("playerJoined", users[socket.id]);
+  socket.emit("playerPositions", users);
 
   socket.on("disconnect", () => {
-    console.log("user disconnected");
+    console.log(`user ${socket.id} disconnected`);
     socket.broadcast.emit("playerLeft", socket.id);
   });
 
-  socket.on("positionUpdate", (positionAdjustment) => {
-    userPositions[socket.id] = [
-      userPositions[socket.id][0] + positionAdjustment[0],
-      userPositions[socket.id][1] + positionAdjustment[1],
-    ];
+  socket.on("positionUpdate", (newPosition: Position) => {
+    users[socket.id] = {
+      id: socket.id,
+      position: {
+        horizontal: newPosition.horizontal,
+        vertical: newPosition.vertical,
+      },
+    };
     console.log(
-      `User ${socket.id} moved to position ${userPositions[socket.id]}`
+      `User ${socket.id} moved to position [${
+        users[socket.id].position.horizontal
+      }, ${users[socket.id].position.vertical}]`
     );
 
-    socket.broadcast.emit(
-      "positionUpdate",
-      socket.id,
-      userPositions[socket.id]
-    );
+    socket.broadcast.emit("positionUpdate", users[socket.id]);
   });
 });
 
 http.listen(8080, () => {
-  console.log("listening on *:8080");
+  console.log("listening on http://localhost:8080");
 });
