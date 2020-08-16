26da83045c4a49811266affb047bdc51c84bd8d3
Dylan Pomeroy <dylan@dylanpomeroy.com>
2020-08-16 17:27:43 -0400

    enhancements to console logs and bug fixes for position updates/sync with server to other clients (#5)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -21,42 +21,38 @@ export default () => {
     horizontal: 0,
     vertical: 0,
   });
-  const [userPositions, setUserPositions] = useState<{ [id: string]: Player }>(
-    {}
-  );
+  const [users, userPositions] = useState<{ [id: string]: Player }>({});
 
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
   useEffect(() => {
-    console.log("called startup useEffect");
     socket.on(
       "playerPositions",
       (playerPositions: { [id: string]: Player }) => {
-        setUserPositions(playerPositions);
+        userPositions(playerPositions);
       }
     );
 
     socket.on("positionUpdate", (player: Player) => {
-      const newUserPositions = { ...userPositions };
-      newUserPositions[player.id] = player;
-      setUserPositions(newUserPositions);
+      const newUsers = { ...users };
+      newUsers[player.id] = player;
+      userPositions(newUsers);
     });
 
     socket.on("playerJoined", (player: Player) => {
-      const newUserPositions = { ...userPositions };
-      newUserPositions[player.id] = player;
-      setUserPositions(newUserPositions);
+      const newUsers = { ...users };
+      newUsers[player.id] = player;
+      userPositions(newUsers);
     });
 
     socket.on("playerLeft", (player: Player) => {
-      const newUserPositions = { ...userPositions };
+      const newUserPositions = { ...users };
       delete newUserPositions[player.id];
-      setUserPositions(newUserPositions);
+      userPositions(newUserPositions);
     });
   }, []);
 
   useEffect(() => {
-    console.log("called useEffect for canvas draw");
     const canvas = canvasRef.current;
     if (!canvas) throw "canvas not found.";
 
@@ -67,9 +63,13 @@ export default () => {
 
     // draw
     drawPlayer(context, myPosition, true);
-    // Object.values(userPositions).forEach((user) =>
-    //   drawPlayer(context, user.position, false)
-    // );
+    Object.values(users).forEach((user) =>
+      drawPlayer(context, user.position, false)
+    );
+  }, [myPosition, users]);
+
+  useEffect(() => {
+    socket.emit("positionUpdate", myPosition);
   }, [myPosition]);
 
   const drawPlayer = (
@@ -88,7 +88,6 @@ export default () => {
   };
 
   const handleKeyPress = (event: React.KeyboardEvent) => {
-    console.log("handleKeyPress");
     let positionUpdate: Position = { horizontal: 0, vertical: 0 };
 
     if (event.keyCode === 39) {
@@ -101,8 +100,6 @@ export default () => {
       positionUpdate = { horizontal: 0, vertical: 1 };
     }
 
-    console.log(positionUpdate);
-    socket.emit("positionUpdate", positionUpdate);
     setMyPosition({
       horizontal: myPosition.horizontal + positionUpdate.horizontal,
       vertical: myPosition.vertical + positionUpdate.vertical,
